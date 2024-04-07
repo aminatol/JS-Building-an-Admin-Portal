81a05189a68290d8200e3abd22e4b820300fcf36
aminatol <aminatoluwo@gmail.com>
2024-04-07 00:54:23 -0400

    added submit quantity to input function

--- public/admin.js
+++ public/admin.js
@@ -1,23 +1,40 @@
-
 // Your Code Here
 //Retrieve a list of books from the server.
 
 async function getBooks() {
- 
-let reponse= await fetch("http://localhost:3001/listBooks")
+  let response = await fetch("http://localhost:3001/listBooks");
 
-let books= await response.json()
+  let books = await response.json();
 
- books.forEach(renderBook)
+  books.forEach(renderBook);
+}
+// Display list of books to admin
+function listBooks(book) {
+  let root = document.querySelector("#root");
+  let list = document.createElement("li");
+  list.textContent = book.title;
 
+  let bookInput = document.createElement("input");
+  bookInput.value = book.quantity;
+  // Place a save button next to each input
+  let submitButton = document.createElement("button");
+  submitButton.textContent = "Save";
+  //When the submit button is clicked, retrieve the quantity from  text input and save the updated quantity to the server.
+  submitButton.addEventListener("click", () => {
+    fetch("http://localhost:3001/updateBook", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: book.id,
+        quanitity: bookInput.value,
+      }),
+    });
+  });
+  list.append(bookInput, submitButton);
+
+  root.append(list);
 }
 
-function listBooks(){
-
-    let root= document.querySelector("#root");
-    let list= document.createElement("li")
-    list.textContent= book.title
-
-    
-
-}
+getBooks();
