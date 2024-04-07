712638d7821390f9b441c2465d0ee60813b88ae8
aminatol <aminatoluwo@gmail.com>
2024-04-06 23:52:22 -0400

    initial commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,23 @@
 
 // Your Code Here
+//Retrieve a list of books from the server.
+
+async function getBooks() {
+ 
+let reponse= await fetch("http://localhost:3001/listBooks")
+
+let books= await response.json()
+
+ books.forEach(renderBook)
+
+}
+
+function listBooks(){
+
+    let root= document.querySelector("#root");
+    let list= document.createElement("li")
+    list.textContent= book.title
+
+    
+
+}
